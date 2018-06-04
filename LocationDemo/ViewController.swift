9392f161f9e7443c8ea00962e01607f4fc9405bd
gdcp <gdcp@m25.local>
2018-06-04 11:39:55 +0800

    picture and mapView

--- LocationDemo/ViewController.swift
+++ LocationDemo/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
-
+import CoreLocation
+import MapKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        /*let manager = CLLocationManager()
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        */
+        let mapView = MKMapView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height))
+        mapView.mapType = .standard
+        mapView.region = MKCoordinateRegion(center: CLLocationCoordinate2DMake(23.16,113.23), span: MKCoordinateSpanMake(1,1))
+        let pn = MKPointAnnotation()
+        pn.coordinate = CLLocationCoordinate2DMake(23.16, 113.23)
+        pn.title = "GuangZhou"
+        pn.subtitle = "GuangDong,China"
+        mapView.addAnnotation(pn)
+        self.view.addSubview(mapView)
+        
+        
     }
-
+   /* func locationManager(_ manager:CLLocationManager, didUpdateLocations locations:[CLLocation]){
+        print(locations)
+    }*/
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
